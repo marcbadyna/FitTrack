49a188fdddd5e973cf6cc2e660f899fe88477caf
marcbadyna <marcbadyna@gmail.com>
2023-12-10 16:05:56 -0500

    Removed expand icon. Made all corners rounded.

--- app/src/main/java/com/example/fittrack/RunCard.kt
+++ app/src/main/java/com/example/fittrack/RunCard.kt
@@ -1,8 +1,5 @@
 package com.example.fittrack
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,18 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -29,18 +20,10 @@ import com.example.fittrack.data.RunData
 
 @Composable
 fun RunCard(runData: RunData, onDelete: (RunData) -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioNoBouncy,
-                    stiffness = Spring.StiffnessMedium
-                )
-            )
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
@@ -54,15 +37,6 @@ fun RunCard(runData: RunData, onDelete: (RunData) -> Unit) {
                 )
                 Spacer(modifier = Modifier.weight(1f))
                 IconButton(
-                    onClick = { expanded = !expanded }
-                ) {
-                    Icon(
-                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.secondary
-                    )
-                }
-                IconButton(
                     onClick = { onDelete(runData) }
                 ) {
                     Icon(
@@ -72,13 +46,10 @@ fun RunCard(runData: RunData, onDelete: (RunData) -> Unit) {
                     )
                 }
             }
-            if (expanded) {
-                RunDetails(runData)
-            }
+            RunDetails(runData)
         }
     }
 }
-
 
 @Composable
 fun RunDetails(runData: RunData) {
@@ -119,6 +90,7 @@ fun RunDetails(runData: RunData) {
         }
     }
 }
+
 
 
 
